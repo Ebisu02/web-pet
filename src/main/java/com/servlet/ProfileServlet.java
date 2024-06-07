3900a9f17ea028a7fb50fa00c1df589e0ed5eb6c
Sergey <helloimclinker@gmail.com>
2024-06-07 17:59:49 +0700

    continue with TODO

--- src/main/java/com/servlet/ProfileServlet.java
+++ src/main/java/com/servlet/ProfileServlet.java
@@ -2,6 +2,7 @@ package com.servlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,5 +14,15 @@ public class ProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String path = /*req.getContextPath() +*/ "/jsp/profile.jsp";
         getServletContext().getRequestDispatcher(path).forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Cookie[] cookies = req.getCookies();
+        for (int i = 0; i < cookies.length; i++) {
+            if (cookies[i].getName().equals("isAuthorized")) {
+                // TODO
+            }
+        }
     }
 }
